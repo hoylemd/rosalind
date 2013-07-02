22ff16167ece7c0d850a5d75db7b50ca086b0db8
hoylemd <hoylemd@gmail.com>
2013-07-01 22:57:28 -0400

    solved gc

--- gc.js
+++ gc.js
@@ -1,15 +1,56 @@
+var processString = function (string) {
+	var tide_counts = 0;
+
+	for(var j in string) {
+		switch(string[j]) {
+			case "C":
+			case "G":
+				tide_counts++;
+				break;
+		}
+	}
+
+	return tide_counts / string.length;
+};
+
 solutions["gc"] = {
 	"program": function (input) {
 		var output = "";
 
 		var lines = input.split("\n");
-		var strings = [];
+		var best_id = ""
+		var best_content = 0;
+		var string = "";
+		var id = "";
 
 		for (var i in lines) {
-			output += lines[i] + "|";
-		};
+			if (lines[i][0] == ">") {
+				if (string) {
+					var content = processString(string);
+					console.log(id);
+					console.log(content);
 
-		return output;
+					if (content > best_content) {
+						best_content = content;
+						best_id = id;
+					}
+				}
+
+				id = lines[i].substring(1);
+				string = ""
+			} else {
+				string += lines[i];
+			}
+		}
+		if (string) {
+			var content = processString(string);
+
+			if (content > best_content) {
+				best_content = content;
+				best_id = id;
+			}
+		}
+		return best_id + "\n" + (best_content * 100);
 	},
 	"label": "5 - Computing GC Content",
 	"id" : "gc"
